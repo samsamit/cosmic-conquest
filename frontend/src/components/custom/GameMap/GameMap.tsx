bead1264e9cb877e0fe1c5ba281eb0d9ebf4e8d7
visma-samu-tiainen <samu.tiainen@visma.com>
2024-01-06 21:11:16 +0200

    Map grid now looks better

--- frontend/src/components/custom/GameMap/GameMap.tsx
+++ frontend/src/components/custom/GameMap/GameMap.tsx
@@ -1,5 +1,4 @@
 import { GameStateStore } from "@/contexts/GameStateContext";
-import { Position } from "@/schemas/gameState.schema";
 import {
   Component,
   For,
@@ -7,7 +6,6 @@ import {
   ParentComponent,
   createEffect,
   createSignal,
-  onMount,
 } from "solid-js";
 import ValueButton from "../buttons/ValueButton";
 import {
@@ -15,7 +13,6 @@ import {
   handleDragScrolling,
 } from "@/utils/handleDragScrolling";
 import EntityCell from "./Entity";
-import { createResizeObserver } from "@solid-primitives/resize-observer";
 
 const GameMap: Component<{
   gameData: NonNullable<GameStateStore["gameData"]>;
@@ -36,23 +33,28 @@ const GameMap: Component<{
   return (
     <>
       <MapContainer onRef={setMapContainerRef}>
-        <MapGrid
-          cellSize={cellSize()}
-          mapHeight={props.gameData.mapHeight}
-          mapWidth={props.gameData.mapWidth}
-        >
-          <For
-            each={getCells(
-              props.gameData.mapWidth ?? 0,
-              props.gameData.mapHeight ?? 0
+        <table class="border-collapse w-fit h-fit relative m-auto">
+          <For each={Array.from({ length: props.gameData.mapHeight })}>
+            {(_) => (
+              <tr>
+                <For each={Array.from({ length: props.gameData.mapWidth })}>
+                  {(_) => (
+                    <td
+                      style={{
+                        height: cellSize() + "px",
+                        width: cellSize() + "px",
+                      }}
+                      class="border border-border"
+                    ></td>
+                  )}
+                </For>
+              </tr>
             )}
-          >
-            {(position) => <Cell></Cell>}
           </For>
           <Index each={props.gameData.entities}>
             {(entity) => <EntityCell entity={entity()} cellSize={cellSize()} />}
           </Index>
-        </MapGrid>
+        </table>
       </MapContainer>
       <ValueButton
         onDecrease={() => handleZoom(-5)}
@@ -60,14 +62,6 @@ const GameMap: Component<{
         class="absolute bottom-4 right-4"
       />
     </>
-  );
-};
-
-const Cell: ParentComponent = (props) => {
-  return (
-    <div class="border border-white w-full h-full flex justify-center items-center">
-      {props.children}
-    </div>
   );
 };
 
@@ -84,34 +78,4 @@ const MapContainer: ParentComponent<{
   );
 };
 
-const MapGrid: ParentComponent<{
-  mapHeight: number;
-  mapWidth: number;
-  cellSize: number;
-}> = (props) => {
-  return (
-    <div
-      class="m-auto relative"
-      style={{
-        display: "grid",
-        "grid-template-columns": `repeat(${props.mapWidth}, ${props.cellSize}px)`,
-        "grid-template-rows": `repeat(${props.mapHeight}, ${props.cellSize}px)`,
-      }}
-    >
-      {props.children}
-    </div>
-  );
-};
-
 export default GameMap;
-
-const getCells = (mapWidth: number, mapHeight: number): Position[] => {
-  const cells: Position[] = [];
-  for (let y = 0; y < mapHeight; y++) {
-    for (let x = 0; x < mapWidth; x++) {
-      const cell = { x, y };
-      cells.push(cell);
-    }
-  }
-  return cells;
-};
